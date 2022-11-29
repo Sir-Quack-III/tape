60afa5090d90bf23ea2dfc68cbccf889f84c1a07
Sir Quack III <77940766+Sir-Quack-III@users.noreply.github.com>
2022-11-29 20:02:35 +0000

    added some things

--- spec.md
+++ spec.md
@@ -3,3 +3,11 @@
 ## Brainfuck, but *slightly* more usable
 
 Tape compiles to brainfuck
+
+```
+    {0,='A'};
+    {1,='B'};
+    {2,='C'};
+    {3,=0};
+    pr!();
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::str::Bytes;
+
+enum TokenType {
+    SET
+}
+
+struct Token {
+    t_type: TokenType,
+    val: String,
+}
+
+fn lex(inp: Bytes) -> Vec<Token> {
+    let out: Vec<Token> = Vec::new();
+    let idx: i32 = 0;
+
+    while idx < inp.len() {
+        
+    }
+
+    return out;
+}
+
 fn main() {
     println!("Hello, world!");
 }
